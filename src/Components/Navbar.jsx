75eafd48a9dfb7fe843e6e2068e951f08aada35e
moichubr <brunmoira@gmail.com>
2023-08-23 18:39:07 -0300

    bugs solved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,10 @@ const Navbar = () => {
     return(
         <header className="bg-indigo-950 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="ml-3 text-xl title-font font-medium text-white mb-4 md:mb-0">
             Moira Brun
           </a>
-        </a>
+       
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 text-white hover:font-bold">
             PROJECTS
