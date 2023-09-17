b9c3949fd523140c1b043d829c389005faab9b02
moichubr <brunmoira@gmail.com>
2023-09-16 23:58:32 -0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import {BiSolidHeartCircle} from "react-icons/bi"
-import { useTranslation } from "react-i18next";
+
 
 const Footer = () => {
-const {t} = useTranslation("global")
 
     return(
-        <div className="bg-indigo-950 text-white font-mono text-lg p-2 lg:w-screen max-w-full">
-            <p className="mb-0">{t('footer.title')} </p>
-            <p>Moira Brun</p>
+        <div className="bg-indigo-950 text-white font-mono text-sm p-2 lg:w-screen max-w-full">
+            <p className="mb-0">Copyright © 2023 Moira Brun </p>
             <div className="inline-block text-rose-600 text-2xl">
             <BiSolidHeartCircle />
             </div>
