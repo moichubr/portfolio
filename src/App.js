52d980eb3e6b4652f01bde7a6fdf1a068f37d942
Moira Brun <97345838+moichubr@users.noreply.github.com>
2024-03-25 17:43:00 -0300

    app routa exito submit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const location = useLocation()
 
     <Suspense fallback="loading...">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/successSubmition' element={<Thankyou />} />
       </Routes>
     </Suspense>
